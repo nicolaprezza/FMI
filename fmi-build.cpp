d64f46db89ab5840787b47fea3ed604aeab89396
nicola <nicolapr@gmail.com>
2016-02-05 17:32:16 +0100

    added possibility to specify SA sample rate

--- fmi-build.cpp
+++ fmi-build.cpp
@@ -12,11 +12,13 @@ using namespace std;
 string out_basename=string();
 string input_file=string();
 
+ulint sr = 9;
 
 void help(){
 	cout << "fmi-build: build FM index of input text file" << endl << endl;
 	cout << "Usage: fmi-build [options] <input_file_name>" << endl;
-	cout << "   -o <basename>         use 'basename' as prefix for all index files. Default: basename is the specified input_file_name"<<endl;
+	cout << "   -o <basename>         use 'basename' as prefix for all index files. Default: basename is the specified input_file_name. Index name will be basename.fmi"<<endl;
+	cout << "   -sr <value>           the suffix array and inverse suffix array sample rates are both set to 2^value. Accepted values: 5,..,10. Default = 9."<<endl;
 	cout << "   <input_file_name>     input text file." << endl;
 	exit(0);
 }
@@ -38,10 +40,38 @@ void parse_args(char** argv, int argc, int &ptr){
 		out_basename = string(argv[ptr]);
 		ptr++;
 
+	}else if(s.compare("-sr")==0){
+
+		if(ptr>=argc-1){
+			cout << "Error: missing parameter after -sr option." << endl;
+			help();
+		}
+
+		sr = atoi(argv[ptr]);
+
+		if(sr<5 or sr>10){
+
+			cout << "Error: -sr accepts values in the range [5,10]." << endl;
+			help();
+
+		}
+
+		ptr++;
+
 	}else{
 		cout << "Error: unrecognized '" << s << "' option." << endl;
 		help();
 	}
+
+}
+
+template<class index_t>
+void build(string input, string output){
+
+	index_t fm_index;
+
+	construct(fm_index, input, 1); // generate index
+	store_to_file(fm_index, output); // save it
 
 }
 
@@ -86,10 +116,18 @@ int main(int argc, char** argv){
 
 	}
 
-	csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> fm_index;
+	switch(sr){
 
-	construct(fm_index, input_file, 1); // generate index
-	store_to_file(fm_index, index_file); // save it
+		case 5: build<csa_wt<wt_huff<rrr_vector<127> >, 32, 32> >(input_file, index_file); break;
+		case 6: build<csa_wt<wt_huff<rrr_vector<127> >, 64, 64> >(input_file, index_file); break;
+		case 7: build<csa_wt<wt_huff<rrr_vector<127> >, 128, 128> >(input_file, index_file); break;
+		case 8: build<csa_wt<wt_huff<rrr_vector<127> >, 256, 256> >(input_file, index_file); break;
+		case 9: build<csa_wt<wt_huff<rrr_vector<127> >, 512, 512> >(input_file, index_file); break;
+		case 10: build<csa_wt<wt_huff<rrr_vector<127> >, 1024, 1024> >(input_file, index_file); break;
+		default: break;
+
+	}
+
 
 	auto t2 = high_resolution_clock::now();
 
--- fmi-search.cpp
+++ fmi-search.cpp
@@ -25,111 +25,158 @@
 using namespace std;
 using namespace sdsl;
 
+bool locate_=false;
+ulint sr=9;
+
 void help(){
 	cout << "fmi-search: count/locate pattern occurrences. Note that this program" << endl;
 	cout << "discards the output (i.e. number of pattern occurrences), and should be used only for benchmark purposes." << endl << endl;
-	cout << "Usage: fmi-search [options] <index_basename> <patterns_file>" << endl;
+	cout << "Usage: fmi-search [options] <index_name> <patterns_file>" << endl;
 	cout << "   -l                  locate occurrences (otherwise only count)" << endl;
+	cout << "   -sr <value>         the suffix array and inverse suffix array sample rates are both set to 2^value. Accepted values: 5,..,10. Default = 9. Note that this value must be the same used during construction."<<endl;
 	cout << "   <index_basename>    basename of all index files" << endl;
 	cout << "   <patterns_file>     file in pizza&chili format containing the patterns." << endl;
 	exit(0);
 }
 
-int main(int argc, char** argv){
+void parse_args(char** argv, int argc, int &ptr){
 
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::duration;
+	assert(ptr<argc);
 
-    if(argc !=4 and argc!=3) help();
+	string s(argv[ptr]);
+	ptr++;
 
-    string index_file;
-    string patterns_file;
-    bool locate = false;
+	if(s.compare("-l")==0){
 
-    if(argc==3){
+		locate_=true;
 
-    	index_file = argv[1];
-    	patterns_file = argv[2];
+	}else if(s.compare("-sr")==0){
 
-    }else{
-
-    	if(string(argv[1]).compare("-l") != 0) help();
-    	locate = true;
-
-    	index_file = argv[2];
-    	patterns_file = argv[3];
-
-    }
-
-    auto t1 = high_resolution_clock::now();
-
-    csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> fm_index;
-    load_from_file(fm_index, index_file);
-
-	auto t2 = high_resolution_clock::now();
-
-	cout << "searching patterns ... " << endl;
-	ifstream ifs(patterns_file);
-
-	//read header of the pizza&chilli input file
-	//header example:
-	//# number=7 length=10 file=genome.fasta forbidden=\n\t
-	string header;
-	std::getline(ifs, header);
-
-	ulint n = get_number_of_patterns(header);
-	ulint m = get_patterns_length(header);
-
-	ulint last_perc = 0;
-
-	ulint occ_tot=0;
-
-	//extract patterns from file and search them in the index
-	for(ulint i=0;i<n;++i){
-
-		ulint perc = (100*i)/n;
-		if(perc>last_perc){
-			cout << perc << "% done ..." << endl;
-			last_perc=perc;
+		if(ptr>=argc-1){
+			cout << "Error: missing parameter after -sr option." << endl;
+			help();
 		}
 
-		string p = string();
+		sr = atoi(argv[ptr]);
 
-		for(ulint j=0;j<m;++j){
-			char c;
-			ifs.get(c);
-			p+=c;
+		if(sr<5 or sr>10){
+
+			cout << "Error: -sr accepts values in the range [5,10]." << endl;
+			help();
+
 		}
 
-		size_t occ = sdsl::count(fm_index, p.begin(), p.end());
+		ptr++;
 
-		if(locate) sdsl::locate(fm_index, p.begin(), p.end());
-
-		occ_tot += occ;
-
+	}else{
+		cout << "Error: unrecognized '" << s << "' option." << endl;
+		help();
 	}
 
-	double occ_avg = (double)occ_tot / n;
+}
 
-	cout << endl << occ_avg << " average occurrences per pattern" << endl;
+template <class index_t>
+void search(string index_file, string patterns_file){
 
-	ifs.close();
+	   using std::chrono::high_resolution_clock;
+	    using std::chrono::duration_cast;
+	    using std::chrono::duration;
 
-	auto t3 = high_resolution_clock::now();
+	    auto t1 = high_resolution_clock::now();
 
-	//printRSSstat();
+	    index_t fm_index;
+	    load_from_file(fm_index, index_file);
 
-	uint64_t load = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-	cout << "Load time : " << load << " milliseconds" << endl;
+		auto t2 = high_resolution_clock::now();
 
-	uint64_t search = std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count();
-	cout << "number of patterns n = " << n << endl;
-	cout << "pattern length m = " << m << endl;
-	cout << "total number of occurrences  occ_t = " << occ_tot << endl;
+		cout << "searching patterns ... " << endl;
+		ifstream ifs(patterns_file);
 
-	cout << "Total time : " << search << " milliseconds" << endl;
-	cout << "Search time : " << (double)search/n << " milliseconds/pattern (total: " << n << " patterns)" << endl;
-	cout << "Search time : " << (double)search/occ_tot << " milliseconds/occurrence (total: " << occ_tot << " occurrences)" << endl;
+		//read header of the pizza&chilli input file
+		//header example:
+		//# number=7 length=10 file=genome.fasta forbidden=\n\t
+		string header;
+		std::getline(ifs, header);
+
+		ulint n = get_number_of_patterns(header);
+		ulint m = get_patterns_length(header);
+
+		ulint last_perc = 0;
+
+		ulint occ_tot=0;
+
+		//extract patterns from file and search them in the index
+		for(ulint i=0;i<n;++i){
+
+			ulint perc = (100*i)/n;
+			if(perc>last_perc){
+				cout << perc << "% done ..." << endl;
+				last_perc=perc;
+			}
+
+			string p = string();
+
+			for(ulint j=0;j<m;++j){
+				char c;
+				ifs.get(c);
+				p+=c;
+			}
+
+			size_t occ = sdsl::count(fm_index, p.begin(), p.end());
+
+			if(locate_) sdsl::locate(fm_index, p.begin(), p.end());
+
+			occ_tot += occ;
+
+		}
+
+		double occ_avg = (double)occ_tot / n;
+
+		cout << endl << occ_avg << " average occurrences per pattern" << endl;
+
+		ifs.close();
+
+		auto t3 = high_resolution_clock::now();
+
+		//printRSSstat();
+
+		uint64_t load = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+		cout << "Load time : " << load << " milliseconds" << endl;
+
+		uint64_t search = std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count();
+		cout << "number of patterns n = " << n << endl;
+		cout << "pattern length m = " << m << endl;
+		cout << "total number of occurrences  occ_t = " << occ_tot << endl;
+
+		cout << "Total time : " << search << " milliseconds" << endl;
+		cout << "Search time : " << (double)search/n << " milliseconds/pattern (total: " << n << " patterns)" << endl;
+		cout << "Search time : " << (double)search/occ_tot << " milliseconds/occurrence (total: " << occ_tot << " occurrences)" << endl;
+
+
+}
+
+int main(int argc, char** argv){
+
+    if(argc <3) help();
+
+	int ptr = 1;
+
+	while(ptr<argc-2)
+		parse_args(argv, argc, ptr);
+
+	string index_file = string(argv[ptr++]);
+	string patterns_file = string(argv[ptr]);
+
+	switch(sr){
+
+		case 5: search<csa_wt<wt_huff<rrr_vector<127> >, 32, 32> >(index_file, patterns_file); break;
+		case 6: search<csa_wt<wt_huff<rrr_vector<127> >, 64, 64> >(index_file, patterns_file); break;
+		case 7: search<csa_wt<wt_huff<rrr_vector<127> >, 128, 128> >(index_file, patterns_file); break;
+		case 8: search<csa_wt<wt_huff<rrr_vector<127> >, 256, 256> >(index_file, patterns_file); break;
+		case 9: search<csa_wt<wt_huff<rrr_vector<127> >, 512, 512> >(index_file, patterns_file); break;
+		case 10: search<csa_wt<wt_huff<rrr_vector<127> >, 1024, 1024> >(index_file, patterns_file); break;
+		default: break;
+
+	}
 
 }
